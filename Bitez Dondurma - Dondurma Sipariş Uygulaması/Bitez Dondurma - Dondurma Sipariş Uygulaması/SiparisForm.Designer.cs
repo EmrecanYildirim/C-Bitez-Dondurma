2a5b08da9389d7b797e7274bd9a699a1f6ecf0c4
EmrecanYildirim <emrecanyildirim35@gmail.com>
2022-09-02 21:22:17 +0300

    Bitez Dondurma Sipariş Programı
    Gümüşlük Bitez Dondurma Şubeşinin Dondurma Sipariş Programıdır.

---
+++ Bitez Dondurma - Dondurma Sipariş Uygulaması/Bitez Dondurma - Dondurma Sipariş Uygulaması/SiparisForm.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Bitez_Dondurma___Dondurma_Sipariş_Uygulaması
+{
+    partial class SiparisForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SiparisForm));
+            this.txtCesit = new System.Windows.Forms.ComboBox();
+            this.txtBoyut = new System.Windows.Forms.ComboBox();
+            this.txtAdet = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCesit
+            // 
+            this.txtCesit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.txtCesit.FormattingEnabled = true;
+            this.txtCesit.Items.AddRange(new object[] {
+            "VANİLYA BADEM",
+            "KESTANE",
+            "TAHİN-SUSAM",
+            "GÜL",
+            "CEVİZ",
+            "ANTEP FISTIĞI",
+            "BAL BADEM",
+            "SAKIZLI",
+            "SADE-SÜTLÜ",
+            "BİTTER",
+            "DİYABETİK",
+            "MANDALİNA",
+            "ÇİLEK",
+            "KAVUN",
+            "MUZ",
+            "ŞEFTALİ",
+            "VİŞNE",
+            "KARADUT",
+            "DETOKS"});
+            this.txtCesit.Location = new System.Drawing.Point(255, 35);
+            this.txtCesit.Name = "txtCesit";
+            this.txtCesit.Size = new System.Drawing.Size(153, 21);
+            this.txtCesit.TabIndex = 0;
+            // 
+            // txtBoyut
+            // 
+            this.txtBoyut.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.txtBoyut.FormattingEnabled = true;
+            this.txtBoyut.Items.AddRange(new object[] {
+            "BÜYÜK BOY",
+            "KÜÇÜK BOY"});
+            this.txtBoyut.Location = new System.Drawing.Point(255, 78);
+            this.txtBoyut.Name = "txtBoyut";
+            this.txtBoyut.Size = new System.Drawing.Size(153, 21);
+            this.txtBoyut.TabIndex = 1;
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.txtAdet.FormattingEnabled = true;
+            this.txtAdet.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.txtAdet.Location = new System.Drawing.Point(255, 121);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(153, 21);
+            this.txtAdet.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(58, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Dondurma Çeşitleri :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(58, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Dondurmanın Boyutu :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(58, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Dondurma Adeti :";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(54, 244);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(630, 204);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(499, 44);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(185, 42);
+            this.btnEkle.TabIndex = 9;
+            this.btnEkle.Text = "Sipariş Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(499, 92);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(185, 42);
+            this.btnSil.TabIndex = 10;
+            this.btnSil.Text = "Sipariş Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Location = new System.Drawing.Point(530, 454);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(154, 42);
+            this.btnGonder.TabIndex = 11;
+            this.btnGonder.Text = "Siparişi Gönder";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.btnGonder_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(52, 487);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Mail Adresi :";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(166, 487);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(165, 20);
+            this.txtMail.TabIndex = 13;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(499, 140);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(185, 42);
+            this.btnGuncelle.TabIndex = 14;
+            this.btnGuncelle.Text = "Siparişi Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(255, 162);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(153, 20);
+            this.dateTimePicker1.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(58, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Sipariş Tarihi :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(58, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Sipariş Sırası :";
+            // 
+            // txtNo
+            // 
+            this.txtNo.Enabled = false;
+            this.txtNo.Location = new System.Drawing.Point(255, 199);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(100, 20);
+            this.txtNo.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(522, 546);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(219, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Sadece Gümüşlük Şubesi İçin Tasarlanmıştır.";
+            // 
+            // SiparisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 568);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnGonder);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAdet);
+            this.Controls.Add(this.txtBoyut);
+            this.Controls.Add(this.txtCesit);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SiparisForm";
+            this.Text = "Gümüşlük Bitez Dondurma - Sipariş Uygulaması ";
+            this.Load += new System.EventHandler(this.SiparisForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox txtCesit;
+        private System.Windows.Forms.ComboBox txtBoyut;
+        private System.Windows.Forms.ComboBox txtAdet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Bitez Dondurma - Dondurma Sipariş Uygulaması/Bitez Dondurma - Dondurma Sipariş Uygulaması/SiparisForm.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Net.Mail;
+
+namespace Bitez_Dondurma___Dondurma_Sipariş_Uygulaması
+{
+    public partial class SiparisForm : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+        public SiparisForm()
+        {
+            InitializeComponent();
+        }
+
+        void SiparisGetir()
+        {
+            baglanti = new SqlConnection("Data Source=DESKTOP-U9FK9EG; Initial Catalog=bitezdondurma; Persist Security Info=False; User ID=sa; Password=1234");
+            baglanti.Open();
+            da = new SqlDataAdapter("SELECT *FROM siparis", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        private void baslikGoster()
+        {
+            dataGridView1.Columns[0].HeaderText = "Sipariş No";
+            dataGridView1.Columns[0].Width = 80;
+            dataGridView1.Columns[0].DefaultCellStyle.Alignment =
+                DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[1].HeaderText = "Çeşit";
+            dataGridView1.Columns[1].Width = 80;
+            dataGridView1.Columns[1].DefaultCellStyle.Alignment =
+                DataGridViewContentAlignment.MiddleLeft;
+            dataGridView1.Columns[2].HeaderText = "Boyut";
+            dataGridView1.Columns[2].Width = 60;
+            dataGridView1.Columns[2].DefaultCellStyle.Alignment =
+                DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[3].HeaderText = "Adet";
+            dataGridView1.Columns[3].Width = 80;
+            dataGridView1.Columns[3].DefaultCellStyle.Alignment =
+                DataGridViewContentAlignment.MiddleRight;
+            dataGridView1.Columns[4].HeaderText = "Sipariş Tarihi";
+            dataGridView1.Columns[4].Width = 120;
+            dataGridView1.Columns[4].DefaultCellStyle.Alignment =
+                DataGridViewContentAlignment.MiddleRight;
+        }
+
+        private void SiparisForm_Load(object sender, EventArgs e)
+        {
+            SiparisGetir();
+            baslikGoster();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtNo.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtCesit.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtBoyut.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtAdet.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            dateTimePicker1.Text=dataGridView1.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string sorgu = "INSERT INTO siparis (cesit,boyut,adet,dtarih) VALUES (@cesit,@boyut,@adet,@dtarih)";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@cesit", txtCesit.Text);
+            komut.Parameters.AddWithValue("@boyut", txtBoyut.Text);
+            komut.Parameters.AddWithValue("@adet", txtAdet.Text);
+            komut.Parameters.AddWithValue("@dtarih", dateTimePicker1.Value);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            SiparisGetir();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            string sorgu = "DELETE FROM siparis WHERE mno=@mno";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@mno", Convert.ToInt32(txtNo.Text));
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            SiparisGetir();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            string sorgu="UPDATE siparis SET cesit=@cesit,boyut=@boyut,adet=@adet,dtarih=@dtarih WHERE mno=@mno";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@mno", Convert.ToInt32(txtNo.Text));
+            komut.Parameters.AddWithValue("@cesit", txtCesit.Text);
+            komut.Parameters.AddWithValue("@boyut", txtBoyut.Text);
+            komut.Parameters.AddWithValue("@adet", txtAdet.Text);
+            komut.Parameters.AddWithValue("@dtarih", dateTimePicker1.Value);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            SiparisGetir();
+
+        }
+
+        private void btnGonder_Click(object sender, EventArgs e)
+        {
+            if(txtMail.Text=="")
+            {
+                MessageBox.Show("Lütfen Mail Adresi Belirtiniz");
+            }
+            else
+            {
+                string mailBody = "<table width='100%' style='border':Solid 1px Black'>";
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    mailBody += "<tr>";
+                    foreach (DataGridViewCell cell  in row.Cells)
+                    {
+                        mailBody += "<td style ='color:blue;'>" + cell.Value + "</td>";
+                    }
+                    mailBody += "</tr>";
+                }
+                mailBody += "</table>";
+
+                MailMessage msj = new MailMessage();
+                msj.To.Add(txtMail.Text);
+                msj.From = new MailAddress("siparisbitezdondurma@gmail.com", "GÜMÜŞLÜK SİPARİŞ");
+                msj.Subject = ("Gümüşlük Bitez Dondurma Güncel Sipariş");
+                msj.IsBodyHtml = true;
+                msj.Body = mailBody;
+
+                SmtpClient Client = new SmtpClient();
+                Client.UseDefaultCredentials = true;
+                Client.Credentials = new System.Net.NetworkCredential("siparisbitezdondurma@gmail.com", "emrecan123");
+                Client.Host = "smtp.gmail.com";
+                Client.Port = 587;
+                Client.EnableSsl = true;
+                Client.Send(msj);
+                msj.Dispose();
+                MessageBox.Show("Siparişiniz Gönderildi");
+                
+            }
+        }
+    }
+}
